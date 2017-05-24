368ead3e5dc33a61edc3d793836a8e7aa4be6a9b
lisamnash <lisa.michelle.nash@gmail.com>
2017-05-24 16:19:46 -0500

    updated settings

--- settings.py
+++ settings.py
@@ -1,14 +1,14 @@
-input_dir = '/Volumes/GetIt/saved_stuff/2017_03_20_several_coil_test/untracked_coil7/coil7n1_2p00_amps.cine'
-
+input_dir = '/Volumes/GetIt/saved_stuff/2017_05_22_fixing_speeds/varying/'
 tracking_settings = {
     'input_dir': input_dir,
-    'output_dir': '/Volumes/GetIt/saved_stuff/2017_03_20_several_coil_test/tracked_coil7/',
-    'first_frame': 400,
+    'output_dir': '/Volumes/GetIt/saved_stuff/varying/',
+    'first_frame': 0,
     'last_frame': -1,
-    'pix': 6,
-    'min_max_radius':[17, 23],
-    # 'tracked_image':-1,
+    'pix': 4,
+    'min_max_radius':[10, 17],
+    #'tracked_image':-1,
     'cf': [0],
-    'min_max_val':[0.0, 1.0]
+    'min_max_val':[0.01, 0.2]
 
     }
+#47065T152
